96dce0a12e201e50ddd412c9cd7d40662ea427c6
Blackstit <56411253+Blackstit@users.noreply.github.com>
2020-01-05 03:49:23 +0100

    update 2.0

--- src/semistral/Semistral.java
+++ src/semistral/Semistral.java
@@ -6,54 +6,67 @@ import java.util.Scanner;
 public class Semistral {
     public static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        inputSecuenses();
+        boolean isGoing = true;
+        String button;
+        
+        OUTER:
+        while (isGoing) {
+            System.out.println("Pokracovat ve spracovani (a/n):");
+            button = sc.next().toLowerCase();
+            switch (button) {
+                case "a":
+                    inputSecuenses();
+                    break;
+                case "n":
+                    System.out.println("Good buy");
+                    break OUTER;
+                default:
+                    System.out.println("Incorrect type. Try again...");
+                    break;
+            }
+        }   
     }
     
-    public static void inputSecuenses (){
+    private static void inputSecuenses (){
+        System.out.println("Zadey prvni posloupnost:");
         ArrayList<Integer> list_1 = new ArrayList<>();
         int num_1 = 0;
         while (num_1 >= 0) {
             num_1 = sc.nextInt();
             list_1.add(num_1);
         }
+        System.out.println("Zadey druhou posloupnost:");
         ArrayList<Integer> list_2 = new ArrayList<>();
         int num_2 = 0;
         while (num_2 >= 0) {
             num_2 = sc.nextInt();
             list_2.add(num_2);
         }
-        outputData(list_1, list_2);
+        mainLogic(list_1, list_2);
     }
     
-    public static void outputData(ArrayList<Integer> list_1, ArrayList<Integer> list_2){
-        System.out.println(list_1.toString());
-        System.out.println(list_2.toString());
+    private static void mainLogic (ArrayList<Integer> list_1, ArrayList<Integer> list_2){
+        int firstSize = list_1.size();
+        int secondSize = list_2.size();
+        int max = 0;
+        
+        int[][] dp = new int[firstSize][secondSize];
+        
+        for (int i = 0; i < firstSize; i++) {
+            for (int j = 0; j < secondSize; j++) { 
+                if(!list_1.get(i).equals(list_2.get(j))){
+		} else {
+                    if(i==0 || j==0){
+                        dp[i][j]=1;
+                    }else{
+                        dp[i][j] = dp[i-1][j-1]+1;
+                    }
+
+                    if(max < dp[i][j])
+                        max = dp[i][j];
+                }
+            }
+        }
+        System.out.println("Nejdelsi spolecny usek obou poslopnosti ma delku: " + max);
     }
-    
-    public static int getLongestCommonSubstring(String a, String b){
-	int m = a.length()-3;
-	int n = b.length()-3;
- 
-	int max = 0;
- 
-	int[][] dp = new int[m][n];
- 
-	for(int i=0; i<m; i++){
-		for(int j=0; j<n; j++){
-			if(a.charAt(i) == b.charAt(j)){
-				if(i==0 || j==0){
-					dp[i][j]=1;
-				}else{
-					dp[i][j] = dp[i-1][j-1]+1;
-				}
- 
-				if(max < dp[i][j])
-					max = dp[i][j];
-			}
- 
-		}
-	}
-	return max;
-    }
-    
 }
